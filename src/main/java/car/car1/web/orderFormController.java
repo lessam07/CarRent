c236d03c840021f394e7fabb91cf41b23d98b2ea
Samsung <lessam07@gmail.com>
2019-02-26 16:36:14 +0600

    first commit

--- src/main/java/car/car1/web/orderFormController.java
+++ src/main/java/car/car1/web/orderFormController.java
@@ -4,9 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/order")
+@RequestMapping("/orders")
 public class orderFormController {
-    @GetMapping("/")
+    @GetMapping("/current")
 public String menu() {
     return "orderForm";
 }
